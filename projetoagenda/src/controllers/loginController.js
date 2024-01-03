dbc1ae25bbd589c5ae49656d36db715ac5d17728
Edson Levi <79099547+devlevis@users.noreply.github.com>
2024-01-03 15:17:47 -0300

    cadastrando usuario

--- projetoagenda/src/controllers/loginController.js
+++ projetoagenda/src/controllers/loginController.js
@@ -1,7 +1,9 @@
 const Login = require('../models/loginModel')
 
 exports.index = (req, res) => {
-    res.render('login')
+    if(req.session.user) return res.render('login-logado')
+
+     return res.render('login')
 }
 exports.register = async (req, res) => {
     try {
@@ -25,4 +27,33 @@ exports.register = async (req, res) => {
         console.log(e)
         res.render('404')
     }
+}
+exports.login = async (req, res) => {
+    try {
+        const login = new Login(req.body)
+        await login.login()
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors)
+            req.session.save(function () {
+                return res.redirect('/login/index')
+            })
+            return
+        }
+
+        req.flash('success', 'Você entrou no sistema!')
+        req.session.user = login.user
+        req.session.save(function () {
+            return res.redirect('/login/index')
+        })
+        
+    } catch (e) {
+        console.log(e)
+        res.render('404')
+    }
+}
+
+exports.logout = function(req, res) {
+    req.session.destroy()
+    res.redirect('/')
 }
--- projetoagenda/src/models/loginModel.js
+++ projetoagenda/src/models/loginModel.js
@@ -15,28 +15,45 @@ class Login {
         this.errors = []
         this.user = null
     }
+
+    async login() {
+        this.valida()
+        if (this.errors.length > 0) return
+        this.user = await LoginModel.findOne({ email: this.body.email })
+
+        if (!this.user) {
+            this.errors.push('Usuario não existe.')
+            return
+        }
+
+        if (!bcryptjs.compareSync(this.body.password, this.user.password)) {
+            this.errors.push('Senha inválida!')
+            this.user = null
+            return
+        }
+
+    }
+
     async register() {
         this.valida()
-        if(this.errors.length > 0) return
+        if (this.errors.length > 0) return
 
         await this.userExists()
 
-        if(this.errors.length > 0) return
+        if (this.errors.length > 0) return
 
         const salt = bcryptjs.genSaltSync()
-            this.body.password = bcryptjs.hashSync(this.body.password, salt)
+        this.body.password = bcryptjs.hashSync(this.body.password, salt)
 
-        try {
-            this.user = await LoginModel.create(this.body) 
-        } catch(e) {
-            console.log(e)
-        }
+        this.user = await LoginModel.create(this.body)
+
     }
 
     async userExists() {
-        const user = await LoginModel.findOne({email: this.body.email})
+        this.user = await LoginModel.findOne({ email: this.body.email })
 
-        if(user) this.errors.push('Esse usuario já existe.')
+        if (this.user) this.errors.push('Esse usuario já existe.')
+
     }
 
     valida() {
@@ -44,7 +61,7 @@ class Login {
         //validação
         //o email precisa ser valido:
         if (!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido')
-        //a senha precisa ter entre 3 e 50 caracteres
+        //a senha precisa ter entre 3 e 50 caracteres:
         if (this.body.password.length < 3 || this.body.password.length > 50) {
             this.errors.push('A senha precisa ter entre 3 e 50 caracteres')
         }
