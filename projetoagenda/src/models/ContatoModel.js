4f42390110b593a24c609501c130f49dbfa75534
Edson Levi <79099547+devlevis@users.noreply.github.com>
2024-01-19 14:42:06 -0300

    delete and list contacts

--- projetoagenda/src/models/ContatoModel.js
+++ projetoagenda/src/models/ContatoModel.js
@@ -17,11 +17,6 @@ function Contato(body) {
     this.contato = null
 }
 
-Contato.buscaPorId = async function(id) {
-    if(typeof id !==  'string') return;
-    const user = await ContatoModel.findById(id)
-    return user
-}
 
 Contato.prototype.register = async function() {
     this.valida()
@@ -63,6 +58,23 @@ Contato.prototype.edit = async function(id) {
     this.valida()
     if(this.errors.length > 0) return
     this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, {new: true})
+}
+
+//Metotodos estaticos(não vão pro prototype):
+Contato.buscaPorId = async function(id) {
+    if(typeof id !==  'string') return;
+    const contato = await ContatoModel.findById(id)
+    return contato
+}
+Contato.buscaContatos = async function(id) {
+    const contatos = await ContatoModel.find()
+    .sort({ criadoEm: -1 })
+    return contatos
+}
+Contato.delete = async function(id) {
+    if(typeof id !==  'string') return;
+    const contato = await ContatoModel.findOneAndDelete(id)
+    return contato
 }
 
 module.exports = Contato
